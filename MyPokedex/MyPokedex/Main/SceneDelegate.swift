992c67a6a4d0257ed9f111bfcf30985e8f52e806
AdriaRos <adria.ros@elma.care>
2022-03-05 12:19:11 +0100

    main
    - Improve window creation

--- MyPokedex/MyPokedex/Main/SceneDelegate.swift
+++ MyPokedex/MyPokedex/Main/SceneDelegate.swift
@@ -15,14 +15,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var container: Factory
     
     override init() {
-        window = UIWindow(frame: UIScreen.main.bounds)
         container = DependencyFactory()
         rootViewController = container.makeTabBar()
         coordinator = MainCoordinator(tabBarController: rootViewController, container: container)
     }
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
         window?.rootViewController = rootViewController
         window?.makeKeyAndVisible()
         coordinator.start()
