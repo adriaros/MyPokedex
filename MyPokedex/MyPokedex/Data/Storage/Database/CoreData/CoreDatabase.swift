7c546dd62888feda00634918d978688a2f6c20cb
AdriaRos <adria.ros@elma.care>
2022-06-10 11:49:42 +0200

    CoreDatabase
    - Added Core Database
    - Added class and protocol

---
+++ MyPokedex/MyPokedex/Data/Storage/Database/CoreData/CoreDatabase.swift
@@ -0,0 +1,13 @@
+//
+//  CoreDatabase.swift
+//  MyPokedex
+//
+//  Created by Adrià Ros on 10/6/22.
+//
+
+import Foundation
+import CoreData
+
+class CoreDatabase: CoreDatabaseProvider {
+    
+}
---
+++ MyPokedex/MyPokedex/Data/Storage/Database/CoreData/Protocol/CoreDatabaseProvider.swift
@@ -0,0 +1,12 @@
+//
+//  CoreDatabaseProvider.swift
+//  MyPokedex
+//
+//  Created by Adrià Ros on 10/6/22.
+//
+
+import Foundation
+
+protocol CoreDatabaseProvider {
+    
+}
